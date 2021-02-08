7d976dbb4d154202630a52618af759a7078816f3
Isaias Berhe <isaiasberhe@gmail.com>
2021-02-07 23:32:14 -0500

    exclamation etc

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          Edit <code>src/App.js</code> and save to reload!!!!.
+          Edit <code>src/App.js</code> and save to reload!!!.
         </p>
-        <a
+        <a>
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        
           Learn React
         </a>
       </header>
